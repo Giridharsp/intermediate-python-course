b5db8e057f2b1605ddb6cfbefb3ff7365687021d
Giridharsp <87273550+Giridharsp@users.noreply.github.com>
2021-07-13 23:38:16 +0530

    Update dice_roller.py

--- dice_roller.py
+++ dice_roller.py
@@ -1,9 +1,17 @@
 import random
 def main():
     dice_rolls = 50
+    roll_sum = 0
     for i in range(0, dice_rolls):
       rnd = random.randint(1, 6)
-      print (f' You rolled a {rnd}' )
+      roll_sum += rnd
+      if rnd == 1:
+       print (f'You rolled a {rnd} Fail' )
+      elif rnd ==6: 
+       print (f'you rolled a {rnd} success')
+      else:
+        print (f'You rolled a {rnd}')
+    print (f'Total roll sum = {roll_sum} ')
 
 if __name__== "__main__":
     main()
