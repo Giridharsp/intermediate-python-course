76d871e9ef42fe25ff2cb8431911490a8286e6c3
Giridharsp <87273550+Giridharsp@users.noreply.github.com>
2021-07-13 16:47:31 +0530

    Update dice_roller.py

--- dice_roller.py
+++ dice_roller.py
@@ -1,5 +1,8 @@
 def main():
-  #print('You rolled a die')
+  print('You rolled a die')
+  import random
+  rnd = radom.randint(1, 6)
+  Print (" Your number is " + str(rnd) )
 
 if __name__== "__main__":
   main()
